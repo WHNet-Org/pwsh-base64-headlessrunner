2436b7b2886e65ad37ed80ca42b7dd20373fa116
Samuel Gagnon <gagnons.infynox@gmail.com>
2022-11-28 15:41:29 -0500

    Fix for pwsh invoke on Linux

--- Go.ps1
+++ Go.ps1
@@ -1,6 +1,3 @@
 . $(Join-Path -Path $(Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath "Init.ps1")
-#$InvocationPath = $MyInvocation.MyCommand.Definition
 
-$Script = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($env:Payload))
-
-powershell -EncodedCommand $env:Payload
+pwsh -EncodedCommand $env:Payload
